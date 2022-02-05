c6f8c3b110ceaa3a28aa7956e2398b695985a008
redapy <baha.redaaaa@gmail.com>
2022-02-05 13:32:42 +0100

    add pages with styles

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+const About = () => {
+    return ( 
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ullam possimus voluptatibus est esse deleniti quo sint. Dicta veritatis dolorem, quae dolor explicabo optio doloribus, dignissimos, recusandae sequi quibusdam ducimus?</p>
+        </div>
+     );
+}
+ 
+export default About;
